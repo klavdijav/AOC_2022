5fa44e107a43061422e77c865404bcc9ce32bd08
Klavdija Veselko <kv8904@student.uni-lj.si>
2022-12-07 00:19:37 +0100

    Shorter read

--- 6/6.py
+++ 6/6.py
@@ -1,6 +1,4 @@
-file = open('data.txt', 'r')
-string = file.read()
-file.close()
-
-print("Part one:", next(i for i in range(len(string)) if len(set(string[i-4:i])) == 4))
-print("Part two:", next(i for i in range(len(string)) if len(set(string[i-14:i])) == 14))
+with open('data.txt') as file:
+    string = file.readline()
+    print("Part one:", next(i for i in range(len(string)) if len(set(string[i-4:i])) == 4))
+    print("Part two:", next(i for i in range(len(string)) if len(set(string[i-14:i])) == 14))
